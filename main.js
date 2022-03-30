a4a25867bb961af388e727eb57f58423ebcc642f
jianshi2 <jianshi2@iflytek.com>
2022-03-30 16:10:24 +0800

    [新增代码] 完成接口

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, shell, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, shell, ipcMain } = require("electron");
+const path = require("path");
 
 function createWindow() {
   // Create the browser window.
@@ -11,23 +11,24 @@ function createWindow() {
     resizable: false,
     maximizable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools({
-    mode: 'bottom'
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
   });
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools({
+  //   mode: "bottom",
+  // });
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
   // and load the index.html of the app.
   // const StaticServer = require('./server')
   // new StaticServer().start()
   // mainWindow.loadURL('http://127.0.0.1:3600/index.html')
-  mainWindow.loadFile('./src/index.html')
-  ipcMain.on('resize', (event, { width, height }) => {
-    mainWindow.setContentSize(width, height)
+  mainWindow.loadFile("./src/index.html");
+  ipcMain.on("resize", (event, { width, height }) => {
+    mainWindow.setContentSize(width, height);
   });
 }
 
@@ -35,21 +36,21 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
+  createWindow();
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
-ipcMain.on('open-url', (event, url) => {
+ipcMain.on("open-url", (event, url) => {
   shell.openExternal(url);
 });
--- src/js/pay.js
+++ src/js/pay.js
@@ -1,2 +1,88 @@
-const { ipcRenderer } = require('electron')
-console.log(ipcRenderer)
+const { ipcRenderer } = require("electron");
+ipcRenderer.send("resize", { height: 610, width: 960 });
+let golbalData = {
+  custom: "http://www.lansaguo.com/",
+  downPrice: 0.01,
+  home: "http://www.lansaguo.com/",
+  installPrice: 0.02,
+};
+let isRemote = false;
+let orderNum = ""; // 订单编号
+let url = ""; // 二维码图片资源
+let timer = null;
+let downUrl = ""; // 支付成功后的下载地址
+$(".type").click(function () {
+  if ($(".container").hasClass("isPay")) {
+    return;
+  }
+  if ($(".container").hasClass("success")) {
+    return;
+  }
+  $(this).addClass("active").siblings().removeClass("active");
+  const flag = $(this).attr("data-flag");
+  isRemote = flag === "1";
+  $("#price").html(isRemote ? golbalData.installPrice : golbalData.downPrice);
+});
+$("#home").click(function () {
+  ipcRenderer.send("open-url", golbalData.home);
+});
+$("#custom").click(function () {
+  ipcRenderer.send("open-url", golbalData.custom);
+});
+$("#pay").click(function () {
+  $(".container").addClass("isPay");
+  const type = isRemote ? 1 : 0;
+  $.ajax({
+    url: "http://www.lansaguo.com/app/pay2/index?type=" + type,
+    dataType: "json",
+    success: function (res) {
+      if ((res.msg = "success")) {
+        orderNum = res.data.orderNum;
+        url = res.data.url;
+        $("#qrcode").attr("src", url);
+        startLoop();
+      }
+    },
+  });
+});
+$(".back").click(function () {
+  $(".container").removeClass("isPay");
+  endLoop();
+});
+$(".download").click(function () {
+  ipcRenderer.send("open-url", downUrl);
+});
+$.ajax({
+  url: "http://www.lansaguo.com/app/pay2/product",
+  dataType: "json",
+  success: function (res) {
+    if ((res.msg = "success")) {
+      golbalData = res.data;
+      $("#price").html(
+        isRemote ? golbalData.installPrice : golbalData.downPrice
+      );
+    }
+  },
+});
+function startLoop() {
+  timer = setInterval(() => {
+    $.ajax({
+      url: "http://www.lansaguo.com/app/pay2/checkOrder?orderNum=" + orderNum,
+      dataType: "json",
+      success: function (res) {
+        if ((res.msg = "success")) {
+          const status = res.data.status;
+          const url = res.data.downUrl;
+          if (status === 1) {
+            endLoop();
+            downUrl = url;
+            $(".container").addClass("success");
+          }
+        }
+      },
+    });
+  }, 1000);
+}
+function endLoop() {
+  clearInterval(timer);
+}
